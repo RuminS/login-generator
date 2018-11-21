23bd67dbfe6f8378db131166b04b3e076e5d657d
SophieR <sophie.rumin@univ-tlse3.fr>
2018-11-21 10:26:17 +0100

    test creation classe de test LoginService

---
+++ src/test/java/geco/LoginServiceTest.java
@@ -0,0 +1,14 @@
+package geco;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class LoginServiceTest {
+
+    
+}
